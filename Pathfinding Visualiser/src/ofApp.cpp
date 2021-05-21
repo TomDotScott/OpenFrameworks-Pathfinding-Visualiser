87b803d8ea4a92388c82c4525055d7b2df618b6c
TomDotScott <tom.scott45456@gmail.com>
2021-05-21 14:47:21 +0100

    Basic Grid
    A basic grid was set up, and when the mouse was clicked, it prints out the row and column of the cell

--- Pathfinding Visualiser/src/ofApp.cpp
+++ Pathfinding Visualiser/src/ofApp.cpp
@@ -1,71 +1,133 @@
 #include "ofApp.h"
 
-//--------------------------------------------------------------
-void ofApp::setup(){
+ofApp::ofApp() :
+	m_cellSizeX(0),
+	m_cellSizeY(0)
+{
 
 }
 
 //--------------------------------------------------------------
-void ofApp::update(){
+void ofApp::setup()
+{
+	const int numCells = 10;
+	m_cellSizeX = ofGetWindowWidth() / numCells;
+	m_cellSizeY = ofGetWindowHeight() / numCells;
+
+	for (int i = 0; i < numCells; i++)
+	{
+		std::vector<ofRectangle> rectangles;
+
+		for (int j = 0; j < numCells; j++)
+		{
+			rectangles.emplace_back(j * m_cellSizeX, i * m_cellSizeY, m_cellSizeX, m_cellSizeY);
+		}
+
+		m_grid.emplace_back(rectangles);
+	}
+}
+
+//--------------------------------------------------------------
+void ofApp::update()
+{
 
 }
 
 //--------------------------------------------------------------
-void ofApp::draw(){
+void ofApp::draw()
+{
+	for (int i = 0; i < m_grid.size(); ++i)
+	{
+		for (int j = 0; j < m_grid[i].size(); ++j)
+		{
+			auto& rect = m_grid[i][j];
+
+			ofSetColor(255, 0, 0);
+			ofNoFill();
+			ofDrawRectangle(rect);
+			
+			ofFill();
+			ofSetColor(255, 255, 255);
+			
+			ofDrawBitmapString(
+				"Row: " + std::to_string(i) + "\nCol: " + std::to_string(j), 
+				{
+					rect.getPosition().x + m_cellSizeX / 2 - 20,
+					rect.getPosition().y + m_cellSizeY / 2
+				}
+			);
+		}
+	}
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key)
+{
 
 }
 
 //--------------------------------------------------------------
-void ofApp::keyPressed(int key){
+void ofApp::keyReleased(int key)
+{
 
 }
 
 //--------------------------------------------------------------
-void ofApp::keyReleased(int key){
+void ofApp::mouseMoved(int x, int y)
+{
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
+void ofApp::mouseDragged(int x, int y, int button)
+{
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
+void ofApp::mousePressed(int x, int y, int button)
+{
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
+void ofApp::mouseReleased(int x, int y, int button)
+{
+	const auto pos = GetXYInGrid({static_cast<float>(x), static_cast<float>(y)});
+	std::cout << pos << std::endl;
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y)
+{
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
+void ofApp::mouseExited(int x, int y)
+{
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y){
+void ofApp::windowResized(int w, int h)
+{
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y){
+void ofApp::gotMessage(ofMessage msg)
+{
 
 }
 
-//--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
-
+ofVec2f ofApp::GetXYInGrid(const ofVec2f screenPosition) const
+{
+	return {screenPosition.y / static_cast<float>(m_cellSizeY), screenPosition.x / static_cast<float>(m_cellSizeX)};
 }
 
 //--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
+void ofApp::dragEvent(ofDragInfo dragInfo)
+{
 
 }
--- Pathfinding Visualiser/src/ofApp.h
+++ Pathfinding Visualiser/src/ofApp.h
@@ -2,23 +2,39 @@
 
 #include "ofMain.h"
 
-class ofApp : public ofBaseApp{
+struct AStarNode
+{
+	int f;
+	int g;
+	int h;
+};
 
-	public:
-		void setup();
-		void update();
-		void draw();
 
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void mouseEntered(int x, int y);
-		void mouseExited(int x, int y);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
-		
+class ofApp : public ofBaseApp
+{
+public:
+	ofApp();
+	void setup();
+	void update();
+	void draw();
+
+	void keyPressed(int key);
+	void keyReleased(int key);
+	void mouseMoved(int x, int y);
+	void mouseDragged(int x, int y, int button);
+	void mousePressed(int x, int y, int button);
+	void mouseReleased(int x, int y, int button);
+	void mouseEntered(int x, int y);
+	void mouseExited(int x, int y);
+	void windowResized(int w, int h);
+	void dragEvent(ofDragInfo dragInfo);
+	void gotMessage(ofMessage msg);
+
+private:
+	int m_cellSizeX;
+	int m_cellSizeY;
+	std::vector<std::vector<ofRectangle>> m_grid;
+
+	void AStarPathFinding(ofVec2f start, ofVec2f end);
+	ofVec2f GetXYInGrid(ofVec2f screenPosition) const;
 };
